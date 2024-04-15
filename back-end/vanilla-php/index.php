2cae03b06ad27e4693ae3840b642e91078f4149e
Matheus Bonadio <matheus_lima_bonadio_xd@hotmail.com>
2024-04-15 11:34:17 -0300

    style: adjusted html icons

--- back-end/vanilla-php/index.php
+++ back-end/vanilla-php/index.php
@@ -7,8 +7,8 @@
     <meta name="description" content="SwaggerUI" />
     <title>Swagger UI</title>
     <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css" />
-    <link rel="icon" type="image/png" href="http://localhost:8000/docs/asset/favicon-32x32.png?v=40d4f2c38d1cd854ad463f16373cbcb6" sizes="32x32" />
-    <link rel="icon" type="image/png" href="http://localhost:8000/docs/asset/favicon-16x16.png?v=f0ae831196d55d8f4115b6c5e8ec5384" sizes="16x16" />
+    <link rel="icon" type="image/png" href="https://unpkg.com/swagger-ui-dist@5.11.0/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="https://unpkg.com/swagger-ui-dist@5.11.0/favicon-16x16.png" sizes="16x16" />
     <style>
         body {
             margin: 0;
